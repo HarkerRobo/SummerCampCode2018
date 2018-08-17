124d6de0111106419ed3c3cf7671c65db9ebbd62
FinnitoProductions <finn@teachmy.com>
2018-08-16 19:04:18 -0700

    Made OI singleton

--- src/main/java/frc/commands/DriveWithVelocityManual.java
+++ src/main/java/frc/commands/DriveWithVelocityManual.java
@@ -3,6 +3,10 @@ package frc.commands;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class DriveWithVelocityManual extends Command {
+    
+    public void execute () {
+        
+    }
     protected boolean isFinished() {
         return false;
     }
--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -3,13 +3,21 @@ package frc.robot;
 import org.harker.robotics.harkerrobolib.wrappers.GamepadWrapper;
 
 public class OI {
+    public static OI oi;
     private static final int DRIVER_PORT = 0;
-    
+
     private GamepadWrapper gamepad = new GamepadWrapper(DRIVER_PORT);
     public static final double CONTROLLER_DEADBAND = 0.1;
 
     public GamepadWrapper getGamepad () {
         return gamepad;
+    }
+
+    public static OI getInstance () {
+        if (oi == null) {
+            oi = new OI();
+        }
+        return oi;
     }
 
 }
