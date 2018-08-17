2bcb360dc9e0f3044921606679b1465cf3aa0e4c
FinnitoProductions <finn@teachmy.com>
2018-08-16 19:02:15 -0700

    Completed gamepad initialization

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -3,7 +3,10 @@ package frc.robot;
 import org.harker.robotics.harkerrobolib.wrappers.GamepadWrapper;
 
 public class OI {
-    private GamepadWrapper gamepad;
+    private static final int DRIVER_PORT = 0;
+    
+    private GamepadWrapper gamepad = new GamepadWrapper(DRIVER_PORT);
+    public static final double CONTROLLER_DEADBAND = 0.1;
 
     public GamepadWrapper getGamepad () {
         return gamepad;
