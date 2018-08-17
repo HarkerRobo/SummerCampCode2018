1ed8ed0bee11eb53068298b5b84bf47a22a8eeac
FinnitoProductions <finn@teachmy.com>
2018-08-16 19:37:32 -0700

    Created timed arm movement command

---
+++ src/main/java/frc/commands/MoveArmTimed.java
@@ -0,0 +1,40 @@
+package frc.commands;
+
+import edu.wpi.first.wpilibj.command.TimedCommand;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+import frc.robot.RobotMap.ArmConstants;
+
+/**
+ * 
+ * @author Finn Frankis
+ * @version Aug 16, 2018
+ */
+public class MoveArmTimed extends TimedCommand {
+
+    private ArmDirection direction;
+
+    /**
+     * Represents which way the arm will move (up corresponds to positive output, down to negative).
+     * @author Finn Frankis
+     * @version Aug 16, 2018
+     */
+    public enum ArmDirection {
+        UP, DOWN
+    }
+
+    /**
+     * Constructs a new RaiseArmTimed.
+     * @param timeout the time for which the arm should be raised
+     */
+    public MoveArmTimed (double timeout, ArmDirection direction) {
+        super(timeout);
+        this.direction = direction;
+    }
+
+    public void execute () {
+        Robot.getArm().setArmOutput((direction == ArmDirection.UP ? ArmConstants.UP_SIGN : -ArmConstants.UP_SIGN)
+                * ArmConstants.TIMED_OUTPUT);
+    }
+
+}
